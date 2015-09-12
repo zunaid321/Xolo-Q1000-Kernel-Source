611fefabdbfd10d55f40d42edff1e4c51694fcba
zunaid321 <zunaid321@gmail.com>
2015-09-13 03:28:12 +0530

    Kernel Source

---
+++ mediatek/build/mbldenv.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# ##########################################################
+# ALPS(Android4.1 based) build environment profile setting
+# ##########################################################
+
+
+# Overwrite PATH environment setting for JDK & arm-eabi if already exists
+export CROSS_COMPILE=/home/zunaid/bq/gcc-4.9.4/bin/arm-cortex_a7-linux-gnueabihf-
+export PATH
+
+
