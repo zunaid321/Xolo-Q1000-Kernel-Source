dd949c4d119ef22b22a739501e414d4fb3cb9b9e
zunaid321 <zunaid321@gmail.com>
2016-02-23 00:08:19 +0600

    Update mbldenv.sh

--- mbldenv.sh
+++ mbldenv.sh
@@ -1,1 +1,9 @@
-mediatek/build/mbldenv.sh
+#!/bin/bash
+# ##########################################################
+# ALPS(Android4.1 based) build environment profile setting
+# ##########################################################
+
+
+# Overwrite PATH environment setting for JDK & arm-eabi if already exists
+export CROSS_COMPILE=/home/zunaid/android/bindu/toolchain/bin/arm-eabi-
+export PATH
